73731b11e6a95bcc0760e493d78026d21d1a9e20
nnnayy <91891306+nnnayy@users.noreply.github.com>
2022-07-26 17:56:36 +0900

    Add files via upload

---
+++ assignment/react04/src/data/information.js
@@ -0,0 +1,13 @@
+const data = {
+    members: [
+      {
+        memId: 1,
+        nickname: "moon bear",
+        name: "서달곰",
+        description: "우리집 귀염 강쥐",
+      }
+    ],
+  };
+  
+  export default data;
+  
---
+++ assignment/react04/src/pages/Moonbear.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Moonbear() {
+  return (
+    <div>Moonbear</div>
+  )
+}
+
+export default Moonbear
